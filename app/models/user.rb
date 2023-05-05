e1aaab0f029bfff624e8ee54f620c9d8c2754518
masakazu-fujimoto <tengbenjianghe@gmail.com>
2023-05-05 14:16:08 +0900

    修正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,5 @@ class User < ApplicationRecord
   validates :last_name_kana,  presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width characters'} 
   validates :first_name,      presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters'} 
   validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width characters'} 
-  validates :date_of_birth,   presence: true
-      
+       
 end
