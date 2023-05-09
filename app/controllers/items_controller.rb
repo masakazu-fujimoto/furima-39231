6e2b04b2b76c4a86d8184852f0f3c19c67f996a4
masakazu-fujimoto <tengbenjianghe@gmail.com>
2023-05-09 15:23:00 +0900

    商品一覧表示機能、編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,]
   def index
+    @items = Item.all.order("created_at DESC")
   end
 
   def new
@@ -15,6 +16,7 @@ class ItemsController < ApplicationController
       render :new
     end    
   end
+
 
   private
   def item_params
